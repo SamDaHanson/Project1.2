1458f85b71fbc9757b1b27bccf258c5f1f8b01d1
Troy Wynn <troynwynn@gmail.com>
2019-01-11 16:27:43 -0500

    New functionality: gif auto-scroll, animated gifs, dynamic div for artist names

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,34 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCNKS3caHsAr6BuWFA9Scrj6KNBGzTnOOc",
+    authDomain: "sam-andrew-troy.firebaseapp.com",
+    databaseURL: "https://sam-andrew-troy.firebaseio.com",
+    projectId: "sam-andrew-troy",
+    storageBucket: "",
+    messagingSenderId: "849715174313"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//Create references
+// const dbDataRef = firebase.database().ref().child('Data');
+// const dbArtistsRef = dbDataRef.child('Artists');
+
+
+var artistsRef = database.ref().child('Artists');
+// var key = artistsRef.push().key;
+
+$("#tracksTitle").hide();
+
 $(document).ready(function () {
 
+    
     var topics = ["Usher", "Foo Fighters", "Green Day", "Black Sabbath", "Kanye", "Cage The Elephant", "Will Smith", "Beyonce"];
 
     function getLyrics(artist) {
-      var artistSearch = artist;
+        $("#tracksTitle").show();
+    var artistSearch = artist;
       document.getElementById("lyrics").textContent = "";
       $.ajax({
         type: "GET",
@@ -45,8 +70,12 @@ $(document).ready(function () {
       });
     }
 
+    console.log($('#backgroundcolor1').height());
+
     function displaySpaceStuff() {
         var space = $(this).attr("data-name");
+        $("#artistName").text(`${space}'s`);
+        // console.log(space);
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + space + "&api_key=mePpseQoZWWEY5RregXq0iDwpYlq2U9J&limit=10";
         getLyrics(space);
         $.ajax({
@@ -54,23 +83,27 @@ $(document).ready(function () {
             method: "GET"
         }).done(function (response) {
             $("#space-value").empty();
-
+            console.log(response.data);
             for (var i = 0; i < response.data.length; i++) {
                 var space = $("<div class='spaceGif'>");
                 var rating = response.data[i].rating;
                 var picRating = $("<p>").text("Rating: " + rating);
                 var picStill = response.data[i].images.fixed_height_still.url;
                 var picAnimate = response.data[i].images.fixed_height.url;
-                var image = $("<img>").addClass("image").attr("src", picStill).attr("data-still", picStill).attr("data-animate", picAnimate).attr("data-state", "still");
+                var image = $("<img>").addClass("image").attr("src", picAnimate).attr("data-still", picStill).attr("data-animate", picAnimate).attr("data-state", "still");
 
                 space.append(image);
-                space.append(picRating);
+                // space.append(picRating);
 
                 if (rating === 'pg' || rating === 'g') {
                     space.append(image);
                     $("#space-value").append(space);
                 }
             }
+
+
+            console.log($("#space-value").height());
+            console.log($("#backgroundcolor1").height());
 
             $(".image").on("click", function () {
                 var state = $(this).attr("data-state");
@@ -83,6 +116,42 @@ $(document).ready(function () {
                     $(this).attr("data-state", "still");
                 }
             });
+
+            if ($('#space-value').height() > ($("#backgroundcolor1").height() - 30)) {
+                setInterval(function () {
+        
+                    start();
+            }, 500); 
+        
+            }
+        
+
+            function animateContent(direction) {  
+            var animationOffset = $('#backgroundcolor1').height() - $('#space-value').height()-30;
+            if (direction == 'up') {
+                animationOffset = 0;
+            }
+        
+            console.log("animationOffset:"+animationOffset);
+            $('#space-value').animate({ "marginTop": (animationOffset)+ "px" }, 25000);
+            }
+        
+            function up(){
+                animateContent("up")
+            }
+            function down(){
+                animateContent("down")
+            }
+        
+            function start(){
+                setTimeout(function () {
+                down();
+            }, 1000);
+                setTimeout(function () {
+                up();
+            }, 1000);
+            }    
+        
         });
     }
 
@@ -98,12 +167,49 @@ $(document).ready(function () {
     }
 
     $("#spaceTheme").on("click", function (event) {
+        
         event.preventDefault();
         var space = $("#space-input").val().trim();
-        topics.push(space);
-        $("#space-input").val(" ");
-        myButtons();
+        var newArtist = {
+            name: space,
+        };
+       
+        artistsRef.update(newArtist);
+
+        if (space != '' && !topics.includes(space)) {
+            topics.push(space);
+            $("#space-input").val(" ");
+            myButtons();
+        }
     });
+
+    // $("#space-value").scroll(function() {
+    //     if ( $("#space-value").scrollTop() > 3000 ) {
+            
+        
+    //         this.scrollBy(0,1);
+    //         // scrolldelay = setTimeout(pageScroll,10);
+    //     // }
+    //         $("#space-value").scrollTop(5);
+    //     }
+    //     else if ( $("#space-value").scrollTop() == 0 ) {
+    //         $("#space-value").scrollTop(3000);
+    //     }    
+
+    //     // $("space-value").scrollTop($("space-value").height());
+    //     // pageScroll();
+    // });
+
+    
+
+    
+
+    
+
+    // function pageScroll() {
+    //     window.scrollBy(0,1);
+    //     scrolldelay = setTimeout(pageScroll,10);
+    // }
 
     $(document).on("click", ".spaceClass", displaySpaceStuff);
     myButtons();
